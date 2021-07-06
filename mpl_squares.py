cb5982f2ab67dda08cd00a4edc77a77f21a63ede
Ken V <ken.valverde@yahoo.com>
2021-07-06 18:33:30 -0500

    Calculate x-axis and y-axis automatically

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,18 +1,23 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values, s=10)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
-ax.set_ylabel("Sqaure of Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
 
 # Set size of tick labels.
 ax.tick_params(axis="both", which="major", labelsize=14)
+
+# set the range for each axis.
+# The axis() method requires four values: the min & max for the x-axis and y-axis
+# x-axis runs from 0 to 1100 & the y-axis runs from 0 to 1,100,000
+ax.axis([0, 1100, 0, 1100000])
 
 plt.show()
